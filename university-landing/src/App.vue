3f84ea5a875a740ccda5a3015e58a32218cbc57d
evgeniaboeva16-ctrl <evgeniaboeva16@gmail.com>
2026-01-29 20:31:36 +0500

    Add files via upload

---
+++ university-landing/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Segoe UI', system-ui, -apple-system, sans-serif;
+}
+
+body {
+  overflow-x: hidden;
+}
+</style>
